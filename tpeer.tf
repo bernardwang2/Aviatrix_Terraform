4a2c5ea8b5063cb08f761e7e28c0b5a9c0488b80
Bernard Wang <bernw7214604@gmail.com>
2018-01-02 17:17:14 -0800

    try

--- tpeer.tf
+++ tpeer.tf
@@ -2,7 +2,7 @@ resource "aviatrix_transpeer" "test_transpeer1" {
   count = "${var.gateways - 1}"
   source = "${element(aviatrix_gateway.VPC-GW.*.id,count.index+1)}"
   nexthop = "${aviatrix_gateway.VPC-GW.0.id}"
-  reachable_cidr = "${aviatrix_gateway.Prem-VPC-GW.vpc_net}"
+  reachable_cidr = "${aviatrix_gateway.On-Prem-VPC-GW.vpc_net}"
   depends_on = ["aviatrix_tunnel.final_tunnel"]
 }
 
